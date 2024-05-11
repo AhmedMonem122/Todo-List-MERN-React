9a8eda51a47b691e1fa51e0fd2b7ad6f263d51d3
AhmedMonem122 <ahmed.monem90@yahoo.com>
2024-05-11 04:05:35 +0300

    **

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import { Toaster } from 'react-hot-toast'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div className='app'>
-    <BrowserRouter>
-    <Routes>
-
-      <Route path="/" element={<Register />} />
-      <Route path="/Login" element={<Login />} />
-
-    </Routes>
-    <Toaster />
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/api/axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://ahmed-monem-todolist-node.onrender.com",
+});
